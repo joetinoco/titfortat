7844fa1e1b41749f72a25c74fda3b505bc0b8e4b
joetinoco <jose.tinoco@gmail.com>
2016-03-08 20:51:29 -0500

    Refactored DB connection code to allow easier reuse in each controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,5 +7,17 @@
 */
 
 exports.render = function(req, res){
-  res.send('Hello, world');
+  var db = require('../models/dbconn')();
+
+  db.query('SHOW TABLES;', function(err, rows, fields) {
+    if (err) throw err;
+    var content = 'Hello, world<br/><hr>DB tables: <br/>';
+    var colHeader = Object.keys(rows[0])[0];
+    for (var i=0; i < rows.length; i++){
+      content += rows[i][colHeader] + '<br/>';
+    }
+    db.end();
+    res.send(content);
+  });
+
 };
--- server.js
+++ server.js
@@ -10,11 +10,9 @@
 console.log('> Loading global app config');
 var config = require('./config/config');
 
-console.log('> Setting up database connection');
-require('./config/mysql')(config);
-
 console.log('> Starting up server');
 var express = require('./config/express');
 var app = express();
+
 app.listen(process.env.PORT);
 console.log('Server (' + process.env.NODE_ENV + ') is up, listening on port ' + process.env.PORT);
